f9b15a6e644f4ba7bcccd236cc07c6418423726d
Tejashwini Bhendarkar <165932676+TejashwiniBhendarkar@users.noreply.github.com>
2024-09-17 22:23:15 +0530

    Codeforces Round 952 (Div. 4)

---
+++ CreatingWord.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CreatingWord {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+       while(t-->0){
+            String str1=sc.next();
+            String str2=sc.next();
+            String nstr1=str2.charAt(0)+str1.substring(1);
+            String nstr2=str1.charAt(0)+str2.substring(1);
+            
+        System.out.println(nstr1+" "+nstr2);
+
+        }
+    
+    }
+}
